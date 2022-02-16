99403cc83bd0702fd1200869dd6df47a92982dc6
Bendik Patrik <bendikp@freemail.hu>
2022-02-16 13:36:14 +0100

    4.feladat

--- SorozatKiir/Program.cs
+++ SorozatKiir/Program.cs
@@ -11,10 +11,12 @@ namespace SorozatKiir
         static void Main(string[] args)
         {
             Console.Write("Adja meg hogy meddig irja ki a program a számokat! ");
-            int szam = Convert.ToInt32(Console.ReadLine());
-            if (szam >= 1)
+            int max = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adja meg hogy mettől irja ki a program a számokat! ");
+            int min = Convert.ToInt32(Console.ReadLine());
+            if (max >= 1 && min >=1 && max > min)
             {
-                for (int i = 1; i <= szam; i++)
+                for (int i = min; i <= max; i++)
                 {
                     Console.WriteLine(i);
                 }
