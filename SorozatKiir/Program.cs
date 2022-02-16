4e2a2951c4fac5dfde0017e639601cbf417e5e31
Bendik Patrik <bendikp@freemail.hu>
2022-02-16 13:40:08 +0100

    2.feladat

--- SorozatKiir/Program.cs
+++ SorozatKiir/Program.cs
@@ -10,21 +10,10 @@ namespace SorozatKiir
     {
         static void Main(string[] args)
         {
-            Console.Write("Adja meg hogy meddig irja ki a program a számokat! ");
-            int max = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Adja meg hogy mettől irja ki a program a számokat! ");
-            int min = Convert.ToInt32(Console.ReadLine());
-            if (max >= 1 && min >=1 && max > min)
-            {
-                for (int i = min; i <= max; i++)
+                for (int i = 1; i <= 10; i++)
                 {
                     Console.WriteLine(i);
                 }
-            }
-            else
-            {
-                Console.WriteLine("Hibás érték");
-            }
         }
     }
 }
