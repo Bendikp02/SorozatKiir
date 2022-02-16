f3cf5569044c2a192714b990cc67e9aadb21070f
Bendik Patrik <bendikp@freemail.hu>
2022-02-16 13:28:39 +0100

    3.feladat

--- SorozatKiir/Program.cs
+++ SorozatKiir/Program.cs
@@ -10,6 +10,19 @@ namespace SorozatKiir
     {
         static void Main(string[] args)
         {
+            Console.Write("Adja meg hogy meddig irja ki a program a számokat! ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            if (szam >= 1)
+            {
+                for (int i = 1; i <= szam; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hibás érték");
+            }
         }
     }
 }
